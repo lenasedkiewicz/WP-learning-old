4b225f4e738bf0f96f3e45cde801352aef7eb7eb
msedkiewicz <magdalenainspirations@gmail.com>
2024-04-04 15:26:55 +0200

    AAdd basic header content

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Fancy Lab</title>
+</head>
+<body <?php body_class(); ?>>
+  <div id="page" class="site">
+    <header>
+      <section class="search">Search</section>
+      <section class="top-bar">
+        <div class="brand">Logo</div>
+        <div class="second-column">
+          <div class="account">Account</div>
+          <nav class="main-menu">Menu</nav>
+        </div>
+      </section>
+    </header>
+  </div>
+</body>
+</html>
