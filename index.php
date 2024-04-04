59e891c6cd9e61f800547f14bb50f02dfce3e716
msedkiewicz <magdalenainspirations@gmail.com>
2024-04-04 15:29:06 +0200

    Add basic content

--- index.php
+++ index.php
@@ -17,6 +17,15 @@
         </div>
       </section>
     </header>
+    <div class="content-area">
+      <main>
+        <section class="slider">Slider</section>
+        <section class="popular-products">Popular Products</section>
+        <section class="new-arrivals">New Arrivals</section>
+        <section class="deal-of-the-week">Deal of the week</section>
+        <section class="lab-blog">News</section>
+      </main>
+    </div>
   </div>
 </body>
 </html>
