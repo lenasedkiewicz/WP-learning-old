bca993c883d32a680ae544a8c05be4359acdb4d5
msedkiewicz <magdalenainspirations@gmail.com>
2024-04-10 22:16:16 +0200

    Add functions basic structure

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Fancy lab functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package FancyLab Woocommerce Course
+*/
+
+function fancy_lab_scripts() {
+  
+}
